0a6381bf9ccedc0236bdfaec90c38a60d6ecca41
bucsi <git@bucsi.net>
2023-08-24 17:57:43 +0300

    ♻️ formatting

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <header>
     <h1>This is the main App component</h1>
-      <nav class="three columns" style="align-items: baseline">
-        <RouterLink to="/" class="primary card">
-          <strong>Krónika</strong>
-        </RouterLink>
-        <RouterLink to="/rovatok" class="primary card">
-          <strong>Rovatok</strong>
-        </RouterLink>
-        <a href="https://kalazanti.github.com" class="accent card">
-          <strong>Aktuális</strong>
-        </a>
+    <nav class="three columns" style="align-items: baseline">
+      <RouterLink to="/" class="primary card">
+        <strong>Krónika</strong>
+      </RouterLink>
+      <RouterLink to="/rovatok" class="primary card">
+        <strong>Rovatok</strong>
+      </RouterLink>
+      <a href="https://kalazanti.github.com" class="accent card">
+        <strong>Aktuális</strong>
+      </a>
     </nav>
   </header>
 
@@ -22,13 +22,19 @@ import { RouterLink, RouterView } from "vue-router";
 import { useArticlesStore } from "@/stores/articles.js";
 
 // import HelloWorld from "./components/HelloWorld.vue";
-import PocketBase from 'pocketbase';
+import PocketBase from "pocketbase";
 
-const pb = new PocketBase('https://kalazanti.fly.dev');
-const articles = useArticlesStore();
+const pb = new PocketBase("https://kalazanti.fly.dev");
+const articlesStore = useArticlesStore();
 
-pb.collection('kronika').getFullList({
-  sort: '-created',
-  filter: "published = true",
-}).then(records => articles.list = records)
+pb.collection("kronika")
+  .getFullList({
+    sort: "-created",
+    filter: "published = true",
+  })
+  .then((records) => records.forEach((record) => {
+    articlesStore.add(record)
+    console.log(record)
+    console.log(articlesStore.length)
+    }));
 </script>
