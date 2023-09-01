64103880338e234cbca6e2f567746dc41a33ffa1
bucsi <git@bucsi.net>
2023-09-01 23:01:45 +0300

    ✨ add TUI Editor wrapper

---
+++ src/components/TUIEditor.vue
@@ -0,0 +1,35 @@
+<!-- source: https://github.com/nhn/tui.editor/issues/2596#issuecomment-1179532418 -->
+<template>
+    <div>
+      <div ref="editor" />
+    </div>
+</template>
+
+<script setup>
+import Editor from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { ref, onMounted } from 'vue';
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: false,
+    default: '',
+  },
+});
+const emit = defineEmits(['update:modelValue']);
+const editor = ref();
+onMounted(() => {
+  const e = new Editor({
+    el: editor.value,
+    height: 'auto',
+    initialEditType: 'wysiwyg',
+    events: {
+      change: () => emit('update:modelValue', e.getHTML()),
+    },
+  });
+});
+</script>
+
+
